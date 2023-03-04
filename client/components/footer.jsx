a7d70e7d73a0998d7c192aa8da0e0aa023c67c66
Junou Lee <junoulee@gmail.com>
2023-03-04 07:41:57 +0000

    touched up a lot of things

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -1,62 +1,29 @@
 import React from 'react';
 import { BsFacebook, BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs';
 import { FiTwitter } from 'react-icons/fi';
+import { FaHome, FaRegEnvelope, FaPhone, FaPrint } from 'react-icons/fa';
+
 export default function Footer() {
   return (
     <div className="footer-container">
       <footer className="text-center text-lg-start bg-white">
-
-        {/* <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom footer">
-
-          <div className="me-5 d-none d-lg-block">
-            <span>Get connected with us on social networks:</span>
-          </div>
-
-          <a href="" className="me-4 link-secondary">
-            <BiCar />
-          </a>
-          <a href="" className="me-4 link-secondary">
-            <BiCar />
-          </a>
-          <a href="" className="me-4 link-secondary">
-            <BiCar />
-          </a>
-          <a href="" className="me-4 link-secondary">
-            <BiCar />
-          </a>
-          <a href="" className="me-4 link-secondary">
-            <BiCar />
-          </a>
-          <a href="" className="me-4 link-secondary">
-            <BiCar />
-          </a>
-
-        </section> */}
-
         <section className="footer">
           <div className="container text-center text-md-start mt-5">
-
             <div className="row mt-3">
-
               <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 mt-5">
-
                 <h6 className="text-uppercase fw-bold mb-4">
                   <img src="/images/logo.png" alt="logo" width="100" height="70" className="d-inline-block align-text-top ms-3 mb-2 me-3" />
                 </h6>
                 <p className="social-media">
-                  <a className="social-icons" href=""><BsFacebook /></a>
-                  <a className="social-icons" href=""><BsInstagram /></a>
-                  <a className="social-icons" href=""><FiTwitter /></a>
-                  <a className="social-icons" href=""><BsLinkedin /></a>
-                  <a className="social-icons" href=""><BsGithub /></a>
+                  <a className="social-icons" href="http://www.facebook.com"><BsFacebook /></a>
+                  <a className="social-icons" href="http://www.instagram.com"><BsInstagram /></a>
+                  <a className="social-icons" href="http://www.twitter.com"><FiTwitter /></a>
+                  <a className="social-icons" href="http://www.linkedin.com"><BsLinkedin /></a>
+                  <a className="social-icons" href="http://www.github.com"><BsGithub /></a>
                 </p>
               </div>
-
               <div className="text-white col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
-                <h6 className="text-uppercase fw-bold mb-4 mt-3">
-                  Products
-                </h6>
+                <h6 className="text-uppercase fw-bold mb-4 mt-3"> About Us</h6>
                 <p>
                   <a href="" className="text-reset">Angular</a>
                 </p>
@@ -70,9 +37,7 @@ export default function Footer() {
                   <a href="" className="text-reset">Laravel</a>
                 </p>
               </div>
-
               <div className="text-white col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
                 <h6 className="text-uppercase fw-bold mb-4 mt-3">
                   Useful links
                 </h6>
@@ -89,21 +54,14 @@ export default function Footer() {
                   <a href="#!" className="text-reset">Help</a>
                 </p>
               </div>
-
               <div className="text-white col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
                 <h6 className="text-uppercase fw-bold mb-4 mt-3">Contact</h6>
-                <p><i className="fas fa-home me-3 text-secondary" /> New York, NY 10012, US</p>
-                <p>
-                  <i className="fas fa-envelope me-3 text-secondary" />
-                  info@example.com
-                </p>
-                <p><i className="fas fa-phone me-3 text-secondary" /> + 01 234 567 88</p>
-                <p><i className="fas fa-print me-3 text-secondary" /> + 01 234 567 89</p>
+                <p><FaHome /> La Habra, CA 90631, USA</p>
+                <p><FaRegEnvelope /> info@example.com</p>
+                <p><FaPhone /> + 01 234 567 88</p>
+                <p><FaPrint /> + 01 234 567 89</p>
               </div>
-
             </div>
-
           </div>
         </section>
 
--- client/components/searchbar.jsx
+++ client/components/searchbar.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
+import { AiOutlineClose } from 'react-icons/ai';
 
 export function SearchBar({ handleSearch, handleInputChange, handleKeyPress }) {
+  const [showCloseButton, setShowCloseButton] = useState(false);
+
+  function toggleX(event) {
+    if (event.target.value === '') {
+      setShowCloseButton(false);
+    } else {
+      setShowCloseButton(true);
+    }
+  }
+
   return (
     <form onSubmit={handleSearch} className="d-flex w-100">
       <div className="input-group flex-grow-1">
         <input type="text"
         name="inputField"
         className="form-control ms-3 border-end-0"
-        onKeyDown={handleKeyPress}
+        onKeyDown={(event) => {
+          handleKeyPress(event);
+          toggleX(event);
+        }}
         onChange={handleInputChange}
         placeholder="Search products" />
+
+        <div className="input-group-append">
+          {showCloseButton && (
+            <button
+              className="btn btn-outline-secondary x-button"
+              type="button"
+              onClick={() => {
+                handleInputChange({ target: { value: '' } });
+                setShowCloseButton(false);
+              }}
+            ><AiOutlineClose /></button>)}
+        </div>
+
         <button type="submit" onClick={handleKeyPress} className="input-group-text me-3 search-button"><BsSearch /></button >
       </div>
+
     </form>
   );
 }
