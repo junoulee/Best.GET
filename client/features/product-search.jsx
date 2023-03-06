25f67ccc1790df4143ce099adc4dced9ff712325
Junou Lee <junoulee@gmail.com>
2023-03-06 22:03:29 +0000

    presentation day

--- client/features/product-search.jsx
+++ client/features/product-search.jsx
@@ -2,11 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { SearchBar } from '../components/searchbar';
 import SearchSuggestions from '../components/search-suggestions';
 
-// Move searchResults to Home
-// Accept prop named "onSearch"
-// When search results are fetched, pass them to onSearch(searchResults)
 export function ProductSearch({ onSearch }) {
-  // const [searchResults, setSearchResults] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [suggestions, setSuggestions] = useState([]);
   const [submitted, setSubmitted] = useState(false);
