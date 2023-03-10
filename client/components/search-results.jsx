40611696cab115ecd2d7ef95082d44b3b5c9c3c7
Junou Lee <junoulee@gmail.com>
2023-03-10 00:11:38 +0000

    more clean-up & deployment prep

--- client/components/search-results.jsx
+++ client/components/search-results.jsx
@@ -4,9 +4,26 @@ import { Link } from 'react-router-dom';
 import NavBar from './navbar';
 import FreeShipping from './free-shipping';
 import Footer from './footer';
+import { css } from '@emotion/react';
+import { RingLoader } from 'react-spinners';
+import styled from 'styled-components';
+
+const SpinnerWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+  `;
 
 export default function SearchResults({ searchTerm }) {
   const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function handleSearch(productName) {
@@ -18,54 +35,65 @@ export default function SearchResults({ searchTerm }) {
         product.category.toLowerCase().includes(searchTerm.toLowerCase())
         );
         setSearchResults(matchingResults);
+        setLoading(false);
       } catch (err) { console.error('Error fetching data:', err); }
     }
     handleSearch();
-  });
+  }, [searchTerm]);
 
   return (
-    <>
-      <NavBar />
-      <FreeShipping />
-      <div className="container search-result-container">
-        <h6 className="results-text fst-italic">{searchResults.length} matching results found</h6>
-      </div>
-      {(searchResults.length === 0) && (
-        <div className="container search-result-container">
-          <div style={{ height: '41.5vh' }} />
-          </div>
-      )}
-
-      <div className="container search-result-container">
-        <div className="row">
-
-          {searchResults.map((result) => (
-            <div className="col-12 col-sm-6 col-md-6 col-lg-3 mb-4" key={result.productId}>
-              <Link to={`/products/${result.productId}`}>
-                <div className="card w-100 result-card">
-                  {/* this button will need to be worked on to be able to route user (cannot be a descendant of the Link above) */}
-                  <button className="heart-favorites bg-white"><FaRegHeart /></button>
-                  <img src={result.imageUrl} className="card-img-top" alt={result.name}/>
-                  <div className="card-body card-body-top">
-                    <h5 className="result-card-title">{result.name}</h5>
-                  </div>
-                  <ul className="list-group list-group-flush">
-                    <li className="list-group-item stars"><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></li>
-                    <li className="list-group-item">{result.manufacturer}</li>
-                    <li className="list-group-item price">
-                      ${Number(result.price).toFixed(2).split('.')[0]}.
-                      <span className="price-decimal">
-                        {Number(result.price).toFixed(2).split('.')[1]}
-                      </span>
-                    </li>
-                  </ul>
-                </div>
-              </Link>
+    <div>
+      {loading
+        ? (
+          <SpinnerWrapper css={override}>
+            <RingLoader color="#36D7B7" loading={loading} size={150} />
+          </SpinnerWrapper>
+          )
+        : (
+          <>
+            <NavBar />
+            <FreeShipping />
+            <div className="container search-result-container">
+              <h6 className="results-text fst-italic">{searchResults.length} matching results found</h6>
             </div>
-          ))}
-        </div>
-      </div>
-      <Footer />
-    </>
+            {(searchResults.length === 0) && (
+            <div className="container search-result-container">
+              <div style={{ height: '41.5vh' }} />
+            </div>
+            )}
+
+            <div className="container search-result-container">
+              <div className="row">
+
+                {searchResults.map((result) => (
+                  <div className="col-12 col-sm-6 col-md-6 col-lg-3 mb-4" key={result.productId}>
+                    <Link to={`/products/${result.productId}`}>
+                      <div className="card w-100 result-card">
+                        {/* this button will need to be worked on to be able to route user (cannot be a descendant of the Link above) */}
+                        <button className="heart-favorites bg-white"><FaRegHeart /></button>
+                        <img src={result.imageUrl} className="card-img-top" alt={result.name}/>
+                        <div className="card-body card-body-top">
+                          <h5 className="result-card-title">{result.name}</h5>
+                        </div>
+                        <ul className="list-group list-group-flush">
+                          <li className="list-group-item stars"><FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></li>
+                          <li className="list-group-item">{result.manufacturer}</li>
+                          <li className="list-group-item price">
+                            ${Number(result.price).toFixed(2).split('.')[0]}.
+                            <span className="price-decimal">
+                              {Number(result.price).toFixed(2).split('.')[1]}
+                            </span>
+                          </li>
+                        </ul>
+                      </div>
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <Footer />
+          </>
+          )}
+    </div>
   );
 }
